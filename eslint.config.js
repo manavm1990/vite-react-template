b5f58a88048b837ae7353db73fc7f019f4a9635b
Manav Misra <manavm@visionwebsoft.com>
2025-10-19 20:09:00 -0500

    Forget about encapsulation of hooks

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import importPlugin from "eslint-plugin-import";
 import perfectionist from "eslint-plugin-perfectionist";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import eslintPluginUseEncapsulation from "eslint-plugin-use-encapsulation";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 
@@ -39,13 +38,6 @@ export default defineConfig([
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
 
       "perfectionist/sort-imports": "error",
-
-      "use-encapsulation/prefer-custom-hooks": [
-        "error",
-        {
-          allow: ["useMemo"],
-        },
-      ],
 
       "no-extra-boolean-cast": "error",
       "no-useless-catch": "error",
