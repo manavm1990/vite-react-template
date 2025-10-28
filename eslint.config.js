5e0a980874d8db5212cb3a1692d24cd2aee953dd
Manav Misra <manavm@visionwebsoft.com>
2025-10-28 16:10:14 -0500

    chore(eslint): :wrench: fix flat config format incompatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,15 @@
 import js from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
 import perfectionist from "eslint-plugin-perfectionist";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginUseEncapsulation from "eslint-plugin-use-encapsulation";
 import globals from "globals";
 
-export default defineConfig([
-  globalIgnores(["dist"]),
+export default [
+  {
+    ignores: ["dist"],
+  },
   {
     files: ["**/*.{js,jsx}"],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -30,6 +25,8 @@ export default defineConfig([
       "use-encapsulation": eslintPluginUseEncapsulation,
     },
     rules: {
+      ...js.configs.recommended.rules,
+
       // Import rules
       "import/first": "error",
       "import/no-duplicates": "error",
@@ -109,6 +106,13 @@ export default defineConfig([
       yoda: "error",
       "no-array-constructor": "error",
       "no-new-object": "error",
+
+      "use-encapsulation/prefer-custom-hooks": [
+        "error",
+        {
+          allow: ["useMemo"],
+        },
+      ],
     },
   },
-]);
+];
